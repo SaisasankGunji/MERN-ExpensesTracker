7de16555e91fdeb882ce38b6006f56f3932395fe
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-03-24 00:20:19 +0530

    Update usersCtrl.js

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -3,57 +3,63 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
-//!User Registration
-
 const usersController = {
-  //!Register
+  //! Register
   register: asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
-    //!Validate
+
+    // Validate
     if (!username || !email || !password) {
       throw new Error("Please all fields are required");
     }
-    //!Check if user already exists
+
+    // Check if user already exists
     const userExists = await User.findOne({ email });
     if (userExists) {
       throw new Error("User already exists");
     }
-    //!Hash the user password
+
+    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    //! Create the user and save into db
+
+    // Create the user
     const userCreated = await User.create({
       email,
       username,
       password: hashedPassword,
     });
-    //! Send the response
 
+    // Send the response
     res.json({
       username: userCreated.username,
       email: userCreated.email,
       id: userCreated._id,
     });
   }),
-  //!Login
+
+  //! Login
   login: asyncHandler(async (req, res) => {
-    //! Get the user data
     const { email, password } = req.body;
-    //!check if email is valid
+
+    // Check if email is valid
     const user = await User.findOne({ email });
     if (!user) {
       throw new Error("Invalid login credentials");
     }
-    //! Compare the user password
+
+    // Compare the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       throw new Error("Invalid login credentials");
     }
-    //! Generate a token
+
+    // Generate a token
     const token = jwt.sign({ id: user._id }, "saisasankKey", {
       expiresIn: "10000d",
     });
-    //!Send the response
+
+    // Send the response
     res.json({
       message: "Login Success",
       token,
@@ -63,49 +69,49 @@ const usersController = {
     });
   }),
 
-  //!profile
+  //! Profile
   profile: asyncHandler(async (req, res) => {
-    //! Find the user
     const user = await User.findById(req.user);
     if (!user) {
       throw new Error("User not found");
     }
-    //!Send the response
     res.json({ username: user.username, email: user.email });
   }),
-  //! Change password
+
+  //! Change Password
   changeUserPassword: asyncHandler(async (req, res) => {
     const { newPassword } = req.body;
-    //! Find the user
+
+    // Find the user
     const user = await User.findById(req.user);
     if (!user) {
       throw new Error("User not found");
     }
-    //! Hash the new password before saving
-    //!Hash the user password
+
+    // Hash the new password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    // Update the password
     user.password = hashedPassword;
-    //! ReSave
-    await user.save({
-      validateBeforeSave: false,
-    });
-    //!Send the response
+    await user.save({ validateBeforeSave: false });
+
+    // Send the response
     res.json({ message: "Password Changed successfully" });
   }),
-  //! update user profile
+
+  //! Update Profile
   updateUserProfile: asyncHandler(async (req, res) => {
     const { email, username } = req.body;
+
+    // Update the user profile
     const updatedUser = await User.findByIdAndUpdate(
       req.user,
-      {
-        username,
-        email,
-      },
-      {
-        new: true,
-      }
+      { username, email },
+      { new: true }
     );
+
+    // Send the response
     res.json({ message: "User profile updated successfully", updatedUser });
   }),
 };
