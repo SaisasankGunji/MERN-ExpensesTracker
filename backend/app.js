0e0582df9c7f8a9b0280c9a1430742d6ab12dce5
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-03-23 23:10:44 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,25 @@ const categoryRouter = require("./routes/categoryRouter");
 const transactionRouter = require("./routes/transactionRouter");
 const app = express();
 
-//Connect to mongodb
+// Connect to MongoDB
 mongoose
   .connect("mongodb+srv://Saisasank:Saisasank%4012@cluster0.y5nye.mongodb.net/mern-expenses")
   .then(() => console.log("DB Connected"))
   .catch((e) => console.log(e));
 
-//! Cors config
+// CORS Configuration
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://mern-expenses-tracker-zeta.vercel.app"], // Allow frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+  credentials: true, // Allow cookies and credentials
 };
+
 app.use(cors(corsOptions));
 
-//!Middlewares
-app.use(express.json()); //?Pass incoming json data
+// Middlewares
+app.use(express.json()); // Parse incoming JSON data
 
-//Routes
+// Routes
 app.use("/", userRouter);
 app.use("/", categoryRouter);
 app.use("/", transactionRouter);
@@ -33,10 +36,10 @@ app.get("/", (req, res) => {
     error: false,
   });
 });
-//! Error
+
+// Error Handler
 app.use(errorHandler);
 
-//Start the server
-
+// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
